68a447cf62cb9b0022e0c9f220da676c85ddc763
enzzodev <enzzocrispim@icloud.com>
2025-04-18 21:53:59 -0300

    Sua mensagem explicando as mudanças feitas

--- secreta.js
+++ secreta.js
@@ -81,3 +81,16 @@ updateCounter();
 ``
 
 
+document.addEventListener("DOMContentLoaded", () => {
+  const music = document.getElementById("bgMusic");
+  const muteButton = document.getElementById("muteButton");
+
+  // Define volume inicial
+  music.volume = 0.1;
+
+  // Ação do botão mute
+  muteButton.addEventListener("click", () => {
+    music.muted = !music.muted;
+    muteButton.textContent = music.muted ? "🔇": "🔊 " ;
+  });
+});
